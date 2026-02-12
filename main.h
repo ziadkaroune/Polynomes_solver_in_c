7ab2b92a8bf1507783a1537339e4c5593a06b617
zivd <ziad99kr@gmail.com>
2026-02-12 14:13:56 +0100

    main.h

---
+++ main.h
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct s_poly {
+double deg_zero ;
+double deg_one ;
+double deg_two ;
+double deg_three;
+}       t_poly;
+
+ 
+
+//utils
+int find_poly_degree(t_poly *coeffi);
+//logic
+void Check_Determinant(t_poly *coeffi , int poly_degree);
+bool isPolynome(char *expression);
+char	*recycle_Polynome(char *eqt , t_poly *coeffi);
