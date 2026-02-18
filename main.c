34bb76c150af196d6d6ef3f55a7d35281d631f27
Ziad <123034104+ziadkaroune@users.noreply.github.com>
2026-02-18 16:05:25 +0100

    Refactor main function for polynomial equation handling

--- main.c
+++ main.c
@@ -1,20 +1,56 @@
 #include "main.h"
 
-
 int main(int ac , char *av[])
 {
-    t_poly coeffi = {0 , 0 , 0 , 0};
-    if(ac != 2 || !isPolynome(av[1])){
-        printf("error arguments");
+    char input[128];
+    char *equation = NULL;
+     int  eqt_degree;
+
+    t_poly coeffi = {false , 0 , 0 , 0 , 0};
+    eqt_degree = 0;
+
+    if(ac > 2){
+        printf("error arguments !\nExample : 5 * X^0 + 4 * X^1 = 4 * X^0\n");
         return -1;
     }
-   char *reducedForm = recycle_Polynome(av[1] , &coeffi);
-   printf("Reduced form : %s\n" , reducedForm);
-   /// fix Reduced Equation
-   printf("Reduced Equation: %.2f * X^2 + %.2f * X + %.2f = 0\n", coeffi.deg_two ,   coeffi.deg_one,  coeffi.deg_zero);
+    else{
+        if (ac == 1) {
+            if (fgets(input, sizeof(input), stdin)) {
+                input[strcspn(input, "\n")] = 0; 
+                equation = input;
+            }
+        }
+         else equation = av[1];
+    }
+   
+    if(!isPolynome(equation)){
+        printf("invalid equation");
+        return -1;
+    }
 
-   int  poly_degree  = find_poly_degree(&coeffi);
-   printf("Polynomial degree : %d\n" , poly_degree);
-   equation_solver(&coeffi , poly_degree);
-    return 0;
+    // intermediate form
+     char *recycle_form = recycle_Polynome(equation , &coeffi);
+      eqt_degree  = find_eqaution_degree(&coeffi);
+      if(eqt_degree > 0)
+            printf("Before Reduced form : %s\n" , recycle_form);
+      else
+            printf("Reduced form : %s\n" , recycle_form);
+
+
+   if(coeffi.heigher_degree){
+        printf("The polynomial degree is strictly greater than 2, I can't solve\n"); 
+        return 0;
+   }
+    
+   char *reduced_form = reducedForm(&coeffi , 300);
+    if(eqt_degree > 0)
+        printf("%s \n" , reduced_form);
+
+   if(eqt_degree > 0)
+        printf("Polynomial degree : %d\n" , eqt_degree);
+   equation_solver(&coeffi , eqt_degree);
+
+   free(recycle_form);
+   free(reduced_form);
+   return 0;
 }
