c3fd355dce381ccdf0df711f27f066553ed8adee
Ziad <123034104+ziadkaroune@users.noreply.github.com>
2026-02-18 16:00:22 +0100

    Refactor output messages in polynomial logic

--- src/poly_logic.c
+++ src/poly_logic.c
@@ -3,24 +3,24 @@
 // Equations degree 0 
 static void zero_degree_eqtion(t_poly *coeffi){
      if (coeffi->deg_zero == 0)
-        printf("All real numbers are solutions\n");
+        printf("Any real number is a solution\n");
     else
-        printf("No solution (contradiction: %.2f = 0)\n", coeffi->deg_zero);
+        printf("No solution.\n");
 }
  
-///  Equations degree 1 
+///  plynome degree 1 
 static void premier_degree_equation(t_poly *coeffi){
      double a =   coeffi->deg_one ;
      double z =   coeffi->deg_zero;
      double result =  0 ;
      
      if(a == 0 && z != 0)
-        printf("- has no solution \n");
+        printf("No solution. \n");
     else if(a == 0 && z == 0)
-         printf("- has infinite solutions \n");
+         printf("Any real number is a solution \n");
     else{
         result = -z/a;
-        printf("- solution : \n %.2f \n" ,  result);
+        printf("solution : \n%.2f \n" ,  result);
     }
 }
 
@@ -31,38 +31,34 @@ static void deuxieme_degree_poly(t_poly *coeffi){
       double b =   coeffi->deg_one ;
       double c =   coeffi->deg_zero;
       
-      double determinant = (b * b) - 4 * a * c;
+      double determinant = (b * b) - 4 * a * c; //(avec a != 0)
       
-      if(a == 0) 
-                printf("a != 0 pour calculer caulculer les solutions");
-      else if(determinant > 0){
-          double x1 = (-b - sqrt(determinant) ) / (2 * a);
-          double x2 = (-b +  sqrt(determinant)) / (2 * a);
-          printf("- Determinant is positive \n the 2 solutions are : %.2f and %.2f \n" , x1 , x2);
+      if(determinant > 0){
+          double x1 = (-b - ft_sqrt(determinant) ) / (2 * a);
+          double x2 = (-b + ft_sqrt(determinant)) / (2 * a);
+          printf("Discriminant is strictly positive, the two  solutions are:\n%.2f\n%.2f \n" , x1 , x2);
       }
             
       else if ( determinant == 0){
           double x  = (-b) / (2 * a);
-            printf("- Determinant = 0 has 1 solution x = %.2f \n" ,   x);
+          printf("Determinant = 0  \n  The solution is:\n %.4f \n" , x);
       }
           
       else{
-           
-           double rx1 = ((-b ) / (2 * a)) ;
-           double imx1 = - (sqrt(-determinant) / (2 * a) );
-            
-            double rx2 = ((-b ) / (2 * a)) ;
-            double imx2 =  (sqrt(-determinant) / (2 * a) );
-         
-          printf("- Determinant is negative , has imaginary solutions \n z1 = %.2f + %.2fi \n z2 = %.2f + %.2fi" ,  rx1 , imx1 , rx2 , imx2 );
+            double rx1 = ((-b ) / (2 * a)) ;
+            double imx1 = -(sqrt(-determinant) / (2 * a) );
+            double rx2 =   rx1 ;
+            double imx2 =  -imx1;
+            printf("Discriminant is strictly negative, the two complex solutions are:\n");
+            printf("z1 = %.2f + (%.2fi) \n" ,  rx1 , imx1 );
+            printf("z2 = %.2f + (%.2fi) \n" , rx2 , imx2 );
       }
-            
 }
 
 // calculate Determinant
 void  equation_solver(t_poly *coeffi , int poly_degree){
     if (poly_degree == -1) // 0 = 0 
-        printf("All real numbers are solutions\n");
+        printf("Any real number is a solution.\n");
     else if(poly_degree == 2)
         deuxieme_degree_poly(coeffi);
     else if(poly_degree == 1)
@@ -70,9 +66,7 @@ void  equation_solver(t_poly *coeffi , int poly_degree){
     else if (poly_degree == 0)
         zero_degree_eqtion(coeffi);
     else{
-         printf("The polynomial degree is strictly greater than 2, I can't solve");
+         printf("The polynomial degree is strictly greater than 2, I can't solve\n");
          return ;
     }
 }
-
-
