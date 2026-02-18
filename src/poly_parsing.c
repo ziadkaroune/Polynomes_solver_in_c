f884057fc58b6d3f3a04fbdfeb8d82e111b38178
Ziad <123034104+ziadkaroune@users.noreply.github.com>
2026-02-18 16:00:59 +0100

    Refactor polynomial parsing and reduce form functions

--- src/poly_parsing.c
+++ src/poly_parsing.c
@@ -31,8 +31,17 @@ static void *store_and_calculate_coeff(char *res , t_poly *coeffi){
                  temp++;
        
              if (*temp == 'X') {
-            
-                        if( *(temp + 1)  == '^' && *(temp + 2) == '2'){
+                        int exponent = ft_atoi(temp + 2);
+                        if( *(temp + 1)  == '^' && exponent > 3){
+                                coeffi->heigher_degree = true;
+                                break;
+                        }
+                            
+                        else if( *(temp + 1)  == '^' && *(temp + 2) == '3'){
+                            coeffi->deg_three += val;
+                            ptr = temp + 3;
+                        }
+                        else if( *(temp + 1)  == '^' && *(temp + 2) == '2'){
                             coeffi->deg_two += val;
                             ptr = temp + 3;
                         }
@@ -75,16 +84,13 @@ char	*recycle_Polynome(char *eqt , t_poly *coeffi){
                  i++; 
                  break;
             } 
-                   
-            else {
+            else 
                  res[j++] = temp[i]; 
-            }
-               
             i++;
     }
     // right side
   
-    while (temp[i] == ' ')  // Skip all leading spaces after '='
+    while (temp[i] == ' ')  // Skip spaces after '='
         i++;
     
     if (temp[i] != '+' && temp[i] != '-')
@@ -102,7 +108,6 @@ char	*recycle_Polynome(char *eqt , t_poly *coeffi){
         i++;
     }
 
-    // (expression)= 0
 	res[j++] = ' '; res[j++] = '='; res[j++] = ' '; res[j++] = '0';
 	res[j] = '\0';
 
@@ -110,3 +115,56 @@ char	*recycle_Polynome(char *eqt , t_poly *coeffi){
      store_and_calculate_coeff(res , coeffi);
 	return (res);
 }
+
+static char handle_sign(double num , bool firstCoef){
+    char sign ;
+           if(num > 0 && !firstCoef) sign = ' ';
+           else if(num > 0) sign = '+';
+           else sign = '-';
+           return sign;
+}
+
+char *reducedForm(t_poly *coeffi , size_t len_eqt ){
+
+    bool isFirst;
+    char sign ;
+    char codi[40];
+
+    char *store = (char *)malloc(sizeof(char) * (len_eqt * 4 + 128));   
+    if (!store)
+        return NULL;
+
+    store[0] = '\0';
+    isFirst = false;
+
+    strcat(store , "Reduced form : ");
+    if(coeffi->deg_three != 0  ){
+          isFirst = true;
+          sprintf(codi ,"%.1f * X^3 ", ft_fabs(coeffi->deg_three ));
+          strcat(store , codi);
+        
+    }
+       if(coeffi->deg_two != 0  ){
+          sign = handle_sign(coeffi->deg_two , isFirst);
+          sprintf(codi ,"%c %1.f * X^2 ", sign ,ft_fabs(coeffi->deg_two));
+          strcat(store , codi);
+           isFirst = true;
+    }
+       if(coeffi->deg_one != 0  ){
+          sign = handle_sign(coeffi->deg_one , isFirst);
+          sprintf(codi ,"%c %.1f * X ", sign , ft_fabs(coeffi->deg_one) );
+          strcat(store , codi);
+          isFirst = true;
+        
+    }
+       if(coeffi->deg_zero){
+          sign = handle_sign(coeffi->deg_zero , isFirst);
+          sprintf(codi ,"%c %.1f ", sign, ft_fabs(coeffi->deg_zero));
+          strcat(store , codi);
+    }
+      strcat(store , " = 0");
+    return store;
+}
+
+
+ 
