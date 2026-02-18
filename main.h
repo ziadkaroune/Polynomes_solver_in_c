6907db030edbdd16158b9a1d26dfc3a560122125
Ziad <123034104+ziadkaroune@users.noreply.github.com>
2026-02-18 16:04:44 +0100

    Clean up includes and rename functions in main.h
    Removed duplicate includes and updated function names.

--- main.h
+++ main.h
@@ -1,18 +1,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 typedef struct s_poly {
+bool   heigher_degree;
 double deg_zero ;
 double deg_one ;
 double deg_two ;
@@ -20,10 +12,13 @@ double deg_three;
 }       t_poly;
 
  
-
 //utils
-int find_poly_degree(t_poly *coeffi);
+int find_eqaution_degree(t_poly *coeffi);
+double ft_fabs(double n);
+double ft_sqrt(double n);
+int ft_atoi(const char *str);
 //logic
 void equation_solver(t_poly *coeffi , int poly_degree);
 bool isPolynome(char *expression);
 char	*recycle_Polynome(char *eqt , t_poly *coeffi);
+char *reducedForm(t_poly *coeffi , size_t len_eqt );
